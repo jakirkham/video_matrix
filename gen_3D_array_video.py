c398e0447ed96aadf0ef6d766d5f9d7ff7132979
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-02 14:51:58 -0500

    gen_3D_array_video.py: Use subplots so as to get axes.

--- gen_3D_array_video.py
+++ gen_3D_array_video.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot
 import matplotlib.animation
 
 # video = 3D numpy array (frames, height, width)
-fig = matplotlib.pyplot.figure()
+fig, axes = matplotlib.pyplot.subplots()
 
 # Amazingly, we can not start with the first frame of our video.
 # Even if we copy the first frame. It also appears to need to be random.
 a = numpy.random.random((video.shape[1], video.shape[2]))
 # Does not work with matshow.
-img = matplotlib.pyplot.imshow(a, interpolation="none", cmap = "gray")
+img = axes.imshow(a, interpolation="none", cmap = "gray")
 
 def init():
     img.set_data(numpy.random.random((video.shape[1], video.shape[2])))
