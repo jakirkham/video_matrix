8ad7de9610005db45b273e90e5a44809610441b5
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-02 14:37:00 -0500

    gen_3D_array_video.py: Removed commented old code.

--- gen_3D_array_video.py
+++ gen_3D_array_video.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plot
 import matplotlib.animation as animation
-#from JSAnimation import HTMLWriter
 
 # video = 3D numpy array (frames, height, width)
 fig = plot.figure()
@@ -19,4 +18,3 @@ def animate(i):
 
 anim = animation.FuncAnimation(fig, animate, init_func = init, frames = len(video), interval = 20, blit = True)
 anim.save("animation.mp4")
-#anim.save("animation.html", writer = HTMLWriter(embed_frames = True))
