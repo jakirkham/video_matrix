cd7204c726c99749554fd65456dc75f3855f6863
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-02 14:41:19 -0500

    gen_3D_array_video.py: Replaced all import as statements with imports and used the modules directly.

--- gen_3D_array_video.py
+++ gen_3D_array_video.py
@@ -1,23 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plot
-import matplotlib.animation as animation
+import numpy
+import matplotlib.pyplot
+import matplotlib.animation
 
 # video = 3D numpy array (frames, height, width)
-fig = plot.figure()
+fig = matplotlib.pyplot.figure()
 
 # Amazingly, we can not start with the first frame of our video.
 # Even if we copy the first frame. It also appears to need to be random.
-a = np.random.random((video.shape[1], video.shape[2]))
+a = numpy.random.random((video.shape[1], video.shape[2]))
 # Does not work with matshow.
-img = plot.imshow(a, interpolation="none", cmap = "gray")
+img = matplotlib.pyplot.imshow(a, interpolation="none", cmap = "gray")
 
 def init():
-    img.set_data(np.random.random((video.shape[1], video.shape[2])))
+    img.set_data(numpy.random.random((video.shape[1], video.shape[2])))
     return [img]
 
 def animate(i):
     img.set_array(video[i])
     return [img]
 
-anim = animation.FuncAnimation(fig, animate, init_func = init, frames = len(video), interval = 50, blit = True)
+anim = matplotlib.animation.FuncAnimation(fig, animate, init_func = init, frames = len(video), interval = 50, blit = True)
 anim.save("animation.mp4")
